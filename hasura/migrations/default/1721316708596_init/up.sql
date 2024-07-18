8cfac3673603bfd7fd2369331b41a8f23deaef42
Luke Policinski <Luke@LukePOLO.com>
2024-07-18 11:32:34 -0400

    feature: add server type on matches (#7)

--- hasura/migrations/default/1721316708596_init/up.sql
+++ hasura/migrations/default/1721316708596_init/up.sql
@@ -311,6 +311,22 @@ BEGIN
 	return password;
 END;
 $$;
+CREATE FUNCTION public.get_match_server_type(match public.matches) RETURNS text
+    LANGUAGE plpgsql STABLE
+    AS $$
+DECLARE
+    is_on_demand BOOL;
+BEGIN
+    IF match.server_id = null THEN
+        return '';
+    END IF;
+	select on_demand into is_on_demand from servers where id = match.server_id;
+	IF is_on_demand = true THEN
+	    return 'OnDemand';
+	END IF;
+	return 'Dedicated';
+END
+$$;
 CREATE TABLE public.teams (
     id uuid DEFAULT gen_random_uuid() NOT NULL,
     name text NOT NULL,
