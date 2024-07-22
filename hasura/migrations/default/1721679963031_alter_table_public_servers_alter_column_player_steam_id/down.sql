ebb458e0bbb2d417fbe3ba9a8460457d83ade947
Luke Policinski <Luke@LukePOLO.com>
2024-07-22 16:27:03 -0400

    wip

---
+++ hasura/migrations/default/1721679963031_alter_table_public_servers_alter_column_player_steam_id/down.sql
@@ -0,0 +1,1 @@
+alter table "public"."servers" rename column "owner_steam_id" to "player_steam_id";
---
+++ hasura/migrations/default/1721679963031_alter_table_public_servers_alter_column_player_steam_id/up.sql
@@ -0,0 +1,1 @@
+alter table "public"."servers" rename column "player_steam_id" to "owner_steam_id";
