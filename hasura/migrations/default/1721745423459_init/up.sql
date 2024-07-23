f7fab70dec7fa1efe82887ef25d68932c92c599d
Luke Policinski <Luke@LukePOLO.com>
2024-07-23 10:37:17 -0400

    wip

--- hasura/migrations/default/1721745423459_init/up.sql
+++ hasura/migrations/default/1721745423459_init/up.sql
@@ -1499,6 +1499,7 @@ CREATE TRIGGER tbiu_update_total_damage_trigger BEFORE INSERT OR UPDATE ON publi
 CREATE TRIGGER tbu_match_player_count BEFORE UPDATE ON public.matches FOR EACH ROW EXECUTE FUNCTION public.tbu_match_player_count();
 CREATE TRIGGER tbu_match_status BEFORE UPDATE ON public.matches FOR EACH ROW EXECUTE FUNCTION public.tbu_match_status();
 CREATE TRIGGER tbui_match_lineup_players BEFORE INSERT OR UPDATE ON public.match_lineup_players FOR EACH ROW EXECUTE FUNCTION public.tbui_match_lineup_players();
+CREATE TRIGGER ti_v_pool_maps INSTEAD OF INSERT ON public.v_pool_maps FOR EACH ROW EXECUTE FUNCTION public.insert_into_v_map_pools();
 ALTER TABLE ONLY public._map_pool
     ADD CONSTRAINT map_pool_map_id_fkey FOREIGN KEY (map_id) REFERENCES public.maps(id) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE ONLY public._map_pool
