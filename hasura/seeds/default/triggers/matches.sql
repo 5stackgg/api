d988a9c822378632a793f7d67eba43650cce1447
Luke Policinski <Luke@LukePOLO.com>
2024-07-30 14:45:23 -0400

    wip

--- hasura/seeds/default/triggers/matches.sql
+++ hasura/seeds/default/triggers/matches.sql
@@ -1,8 +1,20 @@
 CREATE OR REPLACE FUNCTION public.tbi_match() RETURNS TRIGGER
     LANGUAGE plpgsql
     AS $$
+DECLARE
+    _lineup_1_id UUID;
+    _lineup_2_id UUID;
 BEGIN
-    PERFORM create_match_lineups(NEW);
+    IF NEW.lineup_1_id IS NULL THEN
+        INSERT INTO match_lineups DEFAULT VALUES RETURNING id INTO _lineup_1_id;
+         NEW.lineup_1_id = _lineup_1_id;
+    END IF;
+
+    IF NEW.lineup_2_id IS NULL THEN
+       INSERT INTO match_lineups DEFAULT VALUES RETURNING id INTO _lineup_2_id;
+       NEW.lineup_2_id = _lineup_2_id;
+    END IF;
+
 	RETURN NEW;
 END;
 $$;
