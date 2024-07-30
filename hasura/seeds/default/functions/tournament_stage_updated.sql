78b51fcfc20827aac030fa0aaa09059760e8a5dd
Luke Policinski <Luke@LukePOLO.com>
2024-07-30 13:43:14 -0400

    wip

--- hasura/seeds/default/functions/tournament_stage_updated.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION public.tournament_stage_updated() RETURNS trigger
-    LANGUAGE plpgsql
-    AS $$
-BEGIN
-   	PERFORM update_tournament_stages(NEW.tournament_id);
-   	RETURN NEW;
-END;
-$$;
--- hasura/seeds/default/triggers/triggers.sql
+++ hasura/seeds/default/triggers/triggers.sql
@@ -24,9 +24,17 @@ $$;
 DROP TRIGGER IF EXISTS tai_teams ON public.teams;
 CREATE TRIGGER tai_teams AFTER INSERT ON public.teams FOR EACH ROW EXECUTE FUNCTION public.tai_teams();
 
+CREATE OR REPLACE FUNCTION public.taiu_tournament_stages() RETURNS TRIGGER
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    PERFORM update_tournament_stages(NEW.tournament_id);
+	RETURN NEW;
+END;
+$$;
 
 DROP TRIGGER IF EXISTS taiu_tournament_stages ON public.tournament_stages;
-CREATE TRIGGER taiu_tournament_stages AFTER INSERT OR UPDATE ON public.tournament_stages FOR EACH ROW EXECUTE FUNCTION public.tournament_stage_updated();
+CREATE TRIGGER taiu_tournament_stages AFTER INSERT OR UPDATE ON public.tournament_stages FOR EACH ROW EXECUTE FUNCTION public.taiu_tournament_stages();
 
 DROP TRIGGER IF EXISTS taiud ON public.tournament_team_roster;
 CREATE TRIGGER taiud AFTER INSERT OR DELETE OR UPDATE ON public.tournament_team_roster FOR EACH ROW EXECUTE FUNCTION public.check_team_eligibility();
