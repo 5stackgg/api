eab205a0e87c966a746b771c89c884d2349cc1b3
Luke Policinski <Luke@LukePOLO.com>
2024-08-27 17:00:03 -0400

    chore: dont run all the time

--- src/main.ts
+++ src/main.ts
@@ -36,11 +36,11 @@ async function bootstrap() {
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // if (process.env.RUN_MIGRATIONS) {
-  const hasura = app.get(HasuraService);
-  await hasura.setup();
-  // process.exit(0);
-  // }
+  if (process.env.RUN_MIGRATIONS) {
+    const hasura = app.get(HasuraService);
+    await hasura.setup();
+    process.exit(0);
+  }
 
   const configService = app.get(ConfigService);
 
