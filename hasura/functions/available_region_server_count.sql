17503fe730dd79a3b62a04c6aa29be25ff623c59
Luke Policinski <Luke@LukePOLO.com>
2024-08-29 14:42:54 -0400

    feature: add server counts for node and regions

---
+++ hasura/functions/available_region_server_count.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION public.available_region_server_count(e_game_server_node_region public.e_game_server_node_regions) RETURNS INT
+    LANGUAGE plpgsql STABLE
+    AS $$
+DECLARE
+    server_count INT;
+BEGIN
+    SELECT COUNT(*)
+        INTO server_count
+        FROM servers s
+        WHERE s.game_server_node_id in(select id from game_server_nodes gsn where gsn.region = e_game_server_node_region.region) and s.reserved_by_match_id IS NULL;
+
+    RETURN server_count;
+END;
+$$;
---
+++ hasura/functions/total_region_server_count.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION public.total_region_server_count(e_game_server_node_region public.e_game_server_node_regions) RETURNS INT
+    LANGUAGE plpgsql STABLE
+    AS $$
+DECLARE
+    server_count INT;
+BEGIN
+    SELECT COUNT(*)
+        INTO server_count
+        FROM servers s
+        WHERE s.game_server_node_id in(select id from game_server_nodes gsn where gsn.region = e_game_server_node_region.region);
+
+    RETURN server_count;
+END;
+$$;
