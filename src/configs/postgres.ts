63f7990e29dc717cfe333dfc1ccfba686eed1de9
Luke Policinski <Luke@LukePOLO.com>
2024-09-09 20:17:52 -0400

    update postgres eenv vars

--- src/configs/postgres.ts
+++ src/configs/postgres.ts
@@ -6,11 +6,11 @@ export default (): {
   postgres: {
     connections: {
       default: {
-        user: process.env.DB_USER || "hasura",
-        password: process.env.DB_PASSWORD || "hasura",
-        host: process.env.DB_HOST || "postgres",
-        port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
-        database: process.env.DB_DATABASE || "hasura",
+        user: process.env.POSTGRES_USER || "hasura",
+        password: process.env.POSTGRES_PASSWORD || "hasura",
+        host: process.env.POSTGRES_HOST || "postgres",
+        port: process.env.POSTGRES_SERVICE_PORT ? parseInt(process.env.POSTGRES_SERVICE_PORT) : undefined,
+        database: process.env.POSTGRES_DB || "hasura",
         statement_timeout: 1000 * 60,
         max: parseInt(process.env.DB_MAX_POOLS || "5"),
       },
