f826fce84588e84ab7a7748a65d9af76821fc376
Luke Policinski <Luke@LukePOLO.com>
2024-09-12 14:25:19 -0400

    feature: add region statuses

--- hasura/functions/region_status.sql
+++ hasura/functions/region_status.sql
@@ -1,21 +1,25 @@
-CREATE OR REPLACE FUNCTION public.region_status(e_game_server_node_region public.e_game_server_node_regions) RETURNS INT
+CREATE OR REPLACE FUNCTION public.region_status(e_game_server_node_region public.e_game_server_node_regions) RETURNS TEXT
     LANGUAGE plpgsql STABLE
     AS $$
 DECLARE
-    server_count INT;
+    status_count INT;
+    online_count INT;
 BEGIN
-    select status from game_server_node_region_status where e_region = e_game_server_node_region.value;
+    SELECT COUNT(*), COUNT(*) FILTER (WHERE status = 'Online')
+    INTO status_count, online_count
+    FROM game_server_nodes
+    WHERE region = e_game_server_node_region.value;
 
-    if count === 0 then
-        return 'N/A';
-    end if;
+    IF status_count = 0 THEN
+        RETURN 'N/A';
+    END IF;
 
-    if all statues === online then 
-        return 'Online';
-    else
-        return 'Partial';
-    end if;
-
-    return 'Offline';
+    IF online_count = status_count THEN
+        RETURN 'Online';
+    ELSIF online_count > 0 THEN
+        RETURN 'Partial';
+    ELSE
+        RETURN 'Offline';
+    END IF;
 END;
 $$;
