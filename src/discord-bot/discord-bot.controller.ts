f76ccc36d9617650e7729fba791f02683bcd1fac
Luke Policinski <Luke@LukePOLO.com>
2024-10-17 00:16:11 -0400

    feature: disocrd invite link

--- src/discord-bot/discord-bot.controller.ts
+++ src/discord-bot/discord-bot.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get, Req, Res } from "@nestjs/common";
 import { Request, Response } from "express";
+import { HasuraService } from "src/hasura/hasura.service";
 
-@Controller("discord-bot")
+@Controller("/")
 export class DiscordBotController {
-  @Get("/")
+  constructor(private readonly hasura: HasuraService) {}
+
+  @Get("/discord-bot")
   public async bot(@Req() request: Request, @Res() response: Response) {
     // https://discordapi.com/permissions.html
     const permissions = `326434581584`;
@@ -11,6 +14,27 @@ export class DiscordBotController {
     return response.redirect(
       302,
       `https://discord.com/oauth2/authorize?client_id=1168695390502141982&permissions=${permissions}&scope=bot%20applications.commands`,
+    );
+  }
+
+  @Get("/discord-invite")
+  public async invite(@Req() request: Request, @Res() response: Response) {
+    const { settings_by_pk } = await this.hasura.query({
+      settings_by_pk: {
+        __args: {
+          name: "discord_invite_link",
+        },
+        value: true,
+      },
+    });
+
+    console.info(settings_by_pk);
+
+    return response.redirect(
+      302,
+      settings_by_pk?.value
+        ? settings_by_pk.value
+        : `https://discord.gg/6xUDQRAaYY`,
     );
   }
 }
