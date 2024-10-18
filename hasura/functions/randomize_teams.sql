0a27fa58d380cb9e95da25a550772b7825779b00
Luke Policinski <Luke@LukePOLO.com>
2024-10-17 20:23:33 -0400

    feature: randomize teams

--- hasura/functions/randomize_teams.sql
+++ hasura/functions/randomize_teams.sql
@@ -3,12 +3,17 @@ RETURNS void
 LANGUAGE plpgsql
 AS $$
 DECLARE
+    match_status TEXT;
     match_lineup_1_id UUID;
     match_lineup_2_id UUID;
 BEGIN
-    SELECT lineup_1_id, lineup_2_id INTO match_lineup_1_id, match_lineup_2_id
+    SELECT status, lineup_1_id, lineup_2_id INTO match_status, match_lineup_1_id, match_lineup_2_id
     FROM matches
     WHERE id = match_id;
+
+    IF  match.status != 'PickingPlayers' THEN
+        RAISE EXCEPTION 'Match is not picking players' USING ERRCODE = '22000';
+    END IF;
 
     WITH randomized_players AS (
         SELECT id, ROW_NUMBER() OVER (ORDER BY RANDOM()) AS rn
@@ -21,12 +26,20 @@ BEGIN
             CASE 
                 WHEN rn % 2 = 1 THEN match_lineup_1_id
                 ELSE match_lineup_2_id
-            END AS new_lineup_id
+            END AS new_lineup_id,
+            ROW_NUMBER() OVER (PARTITION BY 
+                CASE 
+                    WHEN rn % 2 = 1 THEN match_lineup_1_id
+                    ELSE match_lineup_2_id
+                END 
+            ORDER BY rn) AS team_rn
         FROM randomized_players
     )
     
     UPDATE match_lineup_players mlp
-    SET match_lineup_id = ta.new_lineup_id
+    SET 
+        match_lineup_id = ta.new_lineup_id,
+        captain = CASE WHEN ta.team_rn = 1 THEN true ELSE false END
     FROM team_assignments ta
     WHERE mlp.id = ta.id;
 END;
