5a984a49983300f371892ad0ea50a7f43fc382c7
Luke Policinski <Luke@LukePOLO.com>
2024-11-05 05:56:49 -0500

    chore: adding health endpoint

--- src/system/system.controller.ts
+++ src/system/system.controller.ts
@@ -1,10 +1,16 @@
 import { Controller } from "@nestjs/common";
 import { SystemService } from "./system.service";
 import { HasuraAction } from "src/hasura/hasura.controller";
+import { Get } from "@nestjs/common";
 
 @Controller("system")
 export class SystemController {
   constructor(private readonly system: SystemService) {}
+
+  @Get("health")
+  public async status() {
+    return "OK";
+  }
 
   @HasuraAction()
   public async updateServices() {
