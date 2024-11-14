cf1c30daa5a019166c292626b1ce3127ce5121a0
Luke Policinski <Luke@LukePOLO.com>
2024-11-14 14:20:47 -0500

    chore: update matches based on status

--- src/type-sense/type-sense.controller.ts
+++ src/type-sense/type-sense.controller.ts
@@ -18,6 +18,7 @@ import { InjectQueue } from "@nestjs/bullmq";
 export class TypeSenseController {
   constructor(
     private readonly cache: CacheService,
+    private readonly hasura: HasuraService,
     private readonly typeSense: TypeSenseService,
     @InjectQueue(TypesenseQueues.TypeSense) private queue: Queue,
   ) {}
@@ -59,6 +60,99 @@ export class TypeSenseController {
           delay: new Date(endOfSanction).getTime() - Date.now() + 1000,
         },
       );
+    }
+
+    if (data.new.type === "ban") {
+      const { match_lineup_players } = await this.hasura.query({
+        match_lineup_players: {
+          __args: {
+            where: {
+              steam_id: {
+                _eq: data.new.player_steam_id,
+              },
+              lineup: {
+                v_match_lineup: {
+                  match: {
+                    status: {
+                      _nin: [
+                        "Canceled",
+                        "Finished",
+                        "Forfeit",
+                        "Tie",
+                        "Surrendered",
+                      ],
+                    },
+                  },
+                },
+              },
+            },
+          },
+          id: true,
+          lineup: {
+            id: true,
+            v_match_lineup: {
+              match: {
+                id: true,
+                status: true,
+                lineup_1_id: true,
+                lineup_2_id: true,
+              },
+            },
+          },
+        },
+      });
+
+      for (const matchLineupPlayer of match_lineup_players) {
+        switch (matchLineupPlayer.lineup.v_match_lineup.match.status) {
+          case "Live":
+            await this.hasura.mutation({
+              update_matches_by_pk: {
+                __args: {
+                  pk_columns: {
+                    id: matchLineupPlayer.lineup.v_match_lineup.match.id,
+                  },
+                  _set: {
+                    status: "Forfeit",
+                    winning_lineup_id:
+                      matchLineupPlayer.lineup.id ===
+                      matchLineupPlayer.lineup.v_match_lineup.match.lineup_1_id
+                        ? matchLineupPlayer.lineup.v_match_lineup.match
+                            .lineup_2_id
+                        : matchLineupPlayer.lineup.v_match_lineup.match
+                            .lineup_1_id,
+                  },
+                },
+                __typename: true,
+              },
+            });
+            break;
+          case "PickingPlayers":
+            await this.hasura.mutation({
+              delete_match_lineup_players_by_pk: {
+                __args: {
+                  id: matchLineupPlayer.id,
+                },
+                __typename: true,
+              },
+            });
+            break;
+          default:
+            await this.hasura.mutation({
+              update_matches_by_pk: {
+                __args: {
+                  pk_columns: {
+                    id: matchLineupPlayer.lineup.v_match_lineup.match.id,
+                  },
+                  _set: {
+                    status: "Canceled",
+                  },
+                },
+                __typename: true,
+              },
+            });
+            break;
+        }
+      }
     }
 
     await this.typeSense.updatePlayer(data.new.player_steam_id as string);
