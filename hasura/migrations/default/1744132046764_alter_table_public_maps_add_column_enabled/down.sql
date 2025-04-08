1bd15bbce6ce8246ca22601acefc577f468c8209
Luke Policinski <luke@lukepolo.com>
2025-04-08 14:33:09 -0400

    feature: map pools

--- hasura/migrations/default/1744132046764_alter_table_public_maps_add_column_enabled/down.sql
+++
@@ -1,4 +0,0 @@
--- Could not auto-generate a down migration.
--- Please write an appropriate down migration for the SQL below:
--- alter table "public"."maps" add column "enabled" boolean
---  not null default 'true';
