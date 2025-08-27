8f0f5846ac4865f96d069e358c43e364437bc0b6
Luke Policinski <Luke@LukePOLO.com>
2025-08-27 18:34:22 -0400

    chore: disable allowing steam relay for lan

--- hasura/triggers/server_regions.sql
+++ hasura/triggers/server_regions.sql
@@ -24,3 +24,23 @@ $$;
 
 DROP TRIGGER IF EXISTS tau_validate_lan_regions ON public.server_regions;
 CREATE TRIGGER tau_validate_lan_regions AFTER UPDATE ON public.server_regions FOR EACH ROW EXECUTE FUNCTION public.tau_validate_lan_regions();
+
+CREATE OR REPLACE FUNCTION public.tbiu_server_regions()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    IF OLD.is_lan = true AND NEW.steam_relay = true AND OLD.steam_relay = false THEN
+        RAISE EXCEPTION 'Cannot enable Steam relay on LAN regions' USING ERRCODE = '22000';
+    END IF;
+    
+    IF NEW.is_lan = true AND NEW.steam_relay = true THEN
+        NEW.steam_relay = false;
+    END IF;
+    
+    RETURN NEW;
+END;
+$$;
+
+DROP TRIGGER IF EXISTS tbiu_server_regions ON public.server_regions;
+CREATE TRIGGER tbiu_server_regions BEFORE INSERT OR UPDATE ON public.server_regions FOR EACH ROW EXECUTE FUNCTION public.tbiu_server_regions();
