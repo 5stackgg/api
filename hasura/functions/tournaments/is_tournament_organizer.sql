e005727d1e893ac3c22e8d706f771550cba0a33e
Luke Policinski <Luke@LukePOLO.com>
2025-09-04 09:56:12 -0400

    chore: update how we validate updates on match options

--- hasura/functions/tournaments/is_tournament_organizer.sql
+++ hasura/functions/tournaments/is_tournament_organizer.sql
@@ -6,6 +6,14 @@ CREATE OR REPLACE FUNCTION public.is_tournament_organizer(
 AS $$
 DECLARE
 BEGIN
+    IF hasura_session ->> 'x-hasura-role' = 'admin' OR hasura_session ->> 'x-hasura-role' = 'administrator' THEN
+        RETURN true;
+    END IF;
+
+    IF hasura_session ->> 'x-hasura-role' = 'tournament_organizer' THEN
+        RETURN true;
+    END IF;
+
     RETURN tournament.organizer_steam_id = (hasura_session ->> 'x-hasura-user-id')::bigint OR EXISTS (
         SELECT 1
         FROM public.tournament_organizers
