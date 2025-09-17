f1693cfb302ac1d1e9be27e216e8c49ecdf4cbb0
Luke Policinski <Luke@LukePOLO.com>
2025-09-17 13:42:49 -0400

    chore: set to true if no build

--- hasura/functions/game-server-node/plugin-supported.sql
+++ hasura/functions/game-server-node/plugin-supported.sql
@@ -1,7 +1,7 @@
 create or replace function game_server_node_plugin_supported(game_server_node game_server_nodes) returns boolean as $$
 begin
   if game_server_node.build_id is null then
-    return false;
+    return true;
   end if;
 
   return game_version_supports_plugin(game_server_node.build_id, game_server_node.pin_plugin_version);
