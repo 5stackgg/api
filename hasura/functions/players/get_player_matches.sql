adb18e1eccea426270aa594dcc944bcd4c71bb51
Luke Policinski <Luke@LukePOLO.com>
2025-09-19 19:58:48 -0400

    chore: query perforance for matches for player

--- hasura/functions/players/get_player_matches.sql
+++ hasura/functions/players/get_player_matches.sql
@@ -6,14 +6,7 @@ BEGIN
     RETURN QUERY
         SELECT DISTINCT m.*
         FROM match_lineup_players mlp
-        INNER JOIN matches m ON m.lineup_1_id = mlp.match_lineup_id
-        WHERE mlp.steam_id = player.steam_id
-        
-        UNION
-        
-        SELECT DISTINCT m.*
-        FROM match_lineup_players mlp
-        INNER JOIN matches m ON m.lineup_2_id = mlp.match_lineup_id
+        INNER JOIN matches m ON (m.lineup_1_id = mlp.match_lineup_id OR m.lineup_2_id = mlp.match_lineup_id)
         WHERE mlp.steam_id = player.steam_id;
 END;
 $$;
@@ -24,22 +17,11 @@ CREATE OR REPLACE FUNCTION public.get_total_player_matches(player public.players
 DECLARE
     total_matches INT;
 BEGIN
-    WITH player_matches AS (
-        SELECT DISTINCT m.id
-        FROM match_lineup_players mlp
-        INNER JOIN matches m ON m.lineup_1_id = mlp.match_lineup_id
-        WHERE mlp.steam_id = player.steam_id
-        
-        UNION
-        
-        SELECT DISTINCT m.id
-        FROM match_lineup_players mlp
-        INNER JOIN matches m ON m.lineup_2_id = mlp.match_lineup_id
-        WHERE mlp.steam_id = player.steam_id
-    )
-    SELECT count(*)
+    SELECT COUNT(DISTINCT m.id)
     INTO total_matches
-    FROM player_matches;
+    FROM match_lineup_players mlp
+    INNER JOIN matches m ON (m.lineup_1_id = mlp.match_lineup_id OR m.lineup_2_id = mlp.match_lineup_id)
+    WHERE mlp.steam_id = player.steam_id;
 
     RETURN total_matches;
 END;
