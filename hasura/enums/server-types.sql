2e51b338c70181b4a644520621e36acd0be1da60
Luke Policinski <luke@lukepolo.com>
2025-10-23 09:01:55 -0400

    bug: forgot to add retake to seeders

--- hasura/enums/server-types.sql
+++ hasura/enums/server-types.sql
@@ -5,5 +5,6 @@ insert into e_server_types ("value", "description") values
     ('Wingman', 'Valve Wingman'),
     ('Deathmatch', 'Valve Deathmatch'),
     ('ArmsRace', 'Valve Arms Race'),
+    ('Retake', 'Valve Retake'),
     ('Custom', 'Custom')
 on conflict(value) do update set "description" = EXCLUDED."description"
