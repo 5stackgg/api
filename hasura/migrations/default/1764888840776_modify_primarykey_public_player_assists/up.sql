bb99709891a3d3d6eb5f55438633c9648770c698
Luke Policinski <luke@lukepolo.com>
2025-12-04 19:53:43 -0500

    bug: remove duplicate player assists

--- hasura/migrations/default/1764888840776_modify_primarykey_public_player_assists/up.sql
+++ hasura/migrations/default/1764888840776_modify_primarykey_public_player_assists/up.sql
@@ -1,3 +1,17 @@
+DELETE FROM player_assists
+WHERE id IN (
+    SELECT id
+    FROM (
+        SELECT id,
+               ROW_NUMBER() OVER (
+                   PARTITION BY match_map_id, round, time, attacker_steam_id, attacked_steam_id
+                   ORDER BY id
+               ) as rn
+        FROM player_assists
+    ) t
+    WHERE t.rn > 1
+);
+
 ALTER TABLE "public"."player_assists" DROP CONSTRAINT "player_assists_pkey";
 
 ALTER TABLE "public"."player_assists"
