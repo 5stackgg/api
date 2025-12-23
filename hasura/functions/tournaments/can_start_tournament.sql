f9619be7745646c4385789345f7a5f35ef3978ce
Luke Policinski <luke@lukepolo.com>
2025-12-23 13:21:43 -0500

    feature: add can start to tournament computed

---
+++ hasura/functions/tournaments/can_start_tournament.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE FUNCTION public.can_start_tournament(
+    tournament public.tournaments,
+    hasura_session json
+)
+RETURNS boolean
+LANGUAGE plpgsql STABLE
+AS $$
+BEGIN
+    IF tournament.status != 'Setup' OR tournament.status = 'RegistrationOpen' OR tournament.status = 'RegistrationClosed' THEN
+        RETURN false;
+    END IF;
+
+    IF NOT tournament_has_min_teams(tournament) THEN
+        RETURN false;
+    END IF;
+
+    IF hasura_session ->> 'x-hasura-role' = 'admin' OR 
+       hasura_session ->> 'x-hasura-role' = 'administrator' OR 
+       hasura_session ->> 'x-hasura-role' = 'tournament_organizer' THEN
+        RETURN true;
+    END IF;
+
+    RETURN tournament.organizer_steam_id = (hasura_session ->> 'x-hasura-user-id')::bigint;
+END;
+$$;
