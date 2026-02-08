769597b27a1e0edf2be58d672a6692ba1ded3eab
Luke Policinski <luke@lukepolo.com>
2026-02-08 17:34:03 -0500

    chore: improve performance for query matches and friends

--- hasura/functions/players/get_player_matches.sql
+++ hasura/functions/players/get_player_matches.sql
@@ -1,14 +1,18 @@
-CREATE OR REPLACE FUNCTION public.get_player_matches(player public.players) RETURNS SETOF public.matches
-    LANGUAGE plpgsql STABLE
-    AS $$
-DECLARE
-BEGIN
-    RETURN QUERY
-        SELECT DISTINCT m.*
-        FROM match_lineup_players mlp
-        INNER JOIN matches m ON (m.lineup_1_id = mlp.match_lineup_id OR m.lineup_2_id = mlp.match_lineup_id)
-        WHERE mlp.steam_id = player.steam_id;
-END;
+CREATE OR REPLACE FUNCTION public.get_player_matches(player public.players)
+RETURNS SETOF public.matches
+LANGUAGE SQL STABLE
+AS $$
+    SELECT DISTINCT m.*
+    FROM match_lineup_players mlp
+    JOIN matches m ON m.lineup_1_id = mlp.match_lineup_id
+    WHERE mlp.steam_id = player.steam_id
+
+    UNION
+
+    SELECT DISTINCT m.*
+    FROM match_lineup_players mlp
+    JOIN matches m ON m.lineup_2_id = mlp.match_lineup_id
+    WHERE mlp.steam_id = player.steam_id;
 $$;
 
 CREATE OR REPLACE FUNCTION public.get_total_player_matches(player players)
---
+++ hasura/migrations/default/1770589601000_perf_get_player_matches_optimization/up.sql
@@ -0,0 +1,2 @@
+CREATE INDEX IF NOT EXISTS idx_matches_created_at
+ON matches(created_at DESC);
