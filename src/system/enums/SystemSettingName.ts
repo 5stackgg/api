8bd976537a166d4e2e84f71f7ad074e03275b6c0
Luke Policinski <luke@lukepolo.com>
2026-03-03 12:49:33 -0500

    wip

---
+++ src/system/enums/SystemSettingName.ts
@@ -0,0 +1,9 @@
+export enum SystemSettingName {
+  Updates = "updates",
+  ChatMessageTtl = "chat_message_ttl",
+  DemoNetworkLimiter = "demo_network_limiter",
+  PublicDefaultModels = "public.default_models",
+  SupportsDiscordBot = "public.supports_discord_bot",
+  SupportsGameServerNodes = "supports_game_server_nodes",
+  SupportsGameServerVersionPinning = "supports_game_server_version_pinning",
+}
