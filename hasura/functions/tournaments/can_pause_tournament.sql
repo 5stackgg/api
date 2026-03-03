7b366506d894760d000cf1e55dca3de07f0aa4f6
Flegma <Flegma@users.noreply.github.com>
2026-03-03 17:50:25 +0100

    feat: add can_pause/can_resume tournament permission functions

---
+++ hasura/functions/tournaments/can_pause_tournament.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FUNCTION public.can_pause_tournament(
+    tournament public.tournaments,
+    hasura_session json
+)
+RETURNS boolean
+LANGUAGE plpgsql STABLE
+AS $$
+BEGIN
+    IF tournament.status != 'Live' THEN
+        RETURN false;
+    END IF;
+
+    IF hasura_session ->> 'x-hasura-role' = 'admin' OR hasura_session ->> 'x-hasura-role' = 'administrator' OR hasura_session ->> 'x-hasura-role' = 'tournament_organizer' THEN
+        RETURN true;
+    END IF;
+
+    RETURN is_tournament_organizer(tournament, hasura_session);
+END;
+$$;
