36ec4652c956b2ff8f4d0799162aff0f74fb7a67
Luke Policinski <luke@lukepolo.com>
2026-04-15 13:10:20 -0400

    bug: fix can schedule match

--- hasura/functions/match/can_schedule_match.sql
+++ hasura/functions/match/can_schedule_match.sql
@@ -6,15 +6,13 @@ DECLARE
     lineup_1_ready boolean;
     lineup_2_ready boolean;
 BEGIN
-    IF (match.status != 'Canceled' AND match.status != 'PickingPlayers' AND match.status != 'Scheduled' AND match.status != 'WaitingForCheckIn' AND match.status != 'Surrendered' AND match.status != 'Forfeit') THEN
+    IF (match.status != 'Canceled' AND match.status != 'PickingPlayers' AND match.status != 'Scheduled' AND match.status != 'Surrendered' AND match.status != 'Forfeit') THEN
       return false;
    END IF;
-
 
     IF NOT check_match_has_min_players(match) THEN
         RETURN false;
     END IF;
-
 
     IF is_match_organizer(match, hasura_session) THEN
         RETURN true;
