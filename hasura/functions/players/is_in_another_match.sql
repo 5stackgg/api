3f978593b8bae0e5fdbe09e4e5d74692e6fbba6d
Luke Policinski <luke@lukepolo.com>
2026-04-18 06:58:00 -0400

    bug: fix tournament paused and letting players play in MM while tournament is paused.

--- hasura/functions/players/is_in_another_match.sql
+++ hasura/functions/players/is_in_another_match.sql
@@ -7,14 +7,24 @@ BEGIN
     RETURN EXISTS (
         SELECT 1
         FROM get_player_matches(player) AS pm
-        WHERE 
-        pm.status = 'Live'
-        or 
-        pm.status = 'Veto'
-        or 
-        pm.status = 'WaitingForCheckIn'
-        or 
-        pm.status = 'WaitingForServer'
+        WHERE
+        (
+            pm.status = 'Live'
+            or
+            pm.status = 'Veto'
+            or
+            pm.status = 'WaitingForCheckIn'
+            or
+            pm.status = 'WaitingForServer'
+        )
+        AND NOT EXISTS (
+            SELECT 1
+            FROM tournament_brackets tb
+            INNER JOIN tournament_stages ts ON ts.id = tb.tournament_stage_id
+            INNER JOIN tournaments t ON t.id = ts.tournament_id
+            WHERE tb.match_id = pm.id
+              AND t.status = 'Paused'
+        )
     );
 END;
 $$
