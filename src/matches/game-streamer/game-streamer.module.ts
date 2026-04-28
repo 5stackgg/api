8e83b6c265c6d68f3bd6ce4ef98aa1c11fd235b3
Luke Policinski <luke@lukepolo.com>
2026-04-28 11:20:57 -0400

    chore remove cache

--- src/matches/game-streamer/game-streamer.module.ts
+++ src/matches/game-streamer/game-streamer.module.ts
@@ -1,13 +1,12 @@
 import { Module } from "@nestjs/common";
 import { GameStreamerService } from "./game-streamer.service";
 import { GameStreamerController } from "./game-streamer.controller";
-import { CacheModule } from "../../cache/cache.module";
 import { HasuraModule } from "../../hasura/hasura.module";
 import { EncryptionModule } from "../../encryption/encryption.module";
 import { loggerFactory } from "../../utilities/LoggerFactory";
 
 @Module({
-  imports: [CacheModule, HasuraModule, EncryptionModule],
+  imports: [HasuraModule, EncryptionModule],
   controllers: [GameStreamerController],
   providers: [GameStreamerService, loggerFactory()],
   exports: [GameStreamerService],
