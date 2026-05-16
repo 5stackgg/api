6a569202c31886b200fcfebb49d9894b5dcd5ad7
Luke Policinski <Luke@LukePOLO.com>
2026-05-16 18:11:15 -0400

    chore: add indexes for clips , tourrnaments , streams and trophies (#224)

---
+++ hasura/migrations/default/1799000000000_add_performance_indexes/down.sql
@@ -0,0 +1,4 @@
+DROP INDEX IF EXISTS public.idx_tournament_trophies_player_steam_id;
+DROP INDEX IF EXISTS public.idx_match_streams_match_id;
+DROP INDEX IF EXISTS public.idx_tournament_team_roster_tournament_team_id;
+DROP INDEX IF EXISTS public.match_clips_match_map_id_visibility_idx;
