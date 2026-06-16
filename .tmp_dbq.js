abd81026d02c2930dba8a4820cc16206834c2e6c
Luke Policinski <luke@lukepolo.com>
2026-06-16 18:00:38 -0400

    wip

--- .tmp_dbq.js
+++
@@ -1,8 +0,0 @@
-const { Client } = require("pg");
-const CONN = "postgresql://hasura:hasura@127.0.0.1:63459/hasura";
-(async () => {
-  const c = new Client({ connectionString: CONN, statement_timeout: 120000 });
-  await c.connect();
-  try { const r = await c.query(process.argv[2]); console.log(JSON.stringify(r.rows, null, 2)); }
-  finally { await c.end(); }
-})().catch(e => { console.error("ERR:", e.message); process.exit(1); });
