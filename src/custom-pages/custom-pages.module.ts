f2693c0a6c766667ba49150d1feb5eaeaf2238d5
Luke Policinski <Luke@LukePOLO.com>
2026-07-19 10:41:16 -0400

    chore: plugin auth (#359)

--- src/custom-pages/custom-pages.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from "@nestjs/common";
-import { CustomPagesController } from "./custom-pages.controller";
-
-@Module({
-  controllers: [CustomPagesController],
-})
-export class CustomPagesModule {}
